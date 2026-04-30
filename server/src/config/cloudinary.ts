9c81345b86799e8bac186d9f8c8eef287eebac7f
Nissan Barua <baruanissan55@gmail.com>
2026-04-30 13:53:05 +0600

    fix: revert cloudinary config to 3 separate env variables to fix SDK crash

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -3,18 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (process.env.CLOUDINARY_URL) {
-  // Manually parse cloudinary://api_key:api_secret@cloud_name
-  const url = process.env.CLOUDINARY_URL.replace('cloudinary://', '');
-  const [credentials, cloud_name] = url.split('@');
-  const [api_key, api_secret] = credentials.split(':');
-  cloudinary.config({ cloud_name, api_key, api_secret });
-} else {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-}
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default cloudinary;
