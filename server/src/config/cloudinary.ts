17534c63d6c7d9b2fd84e87e6b8d6be547e575a8
Nissan Barua <baruanissan55@gmail.com>
2026-04-30 13:33:17 +0600

    feat: support CLOUDINARY_URL in cloudinary config

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+if (process.env.CLOUDINARY_URL) {
+  // If CLOUDINARY_URL is present, the SDK handles config automatically
+  cloudinary.config(true);
+} else {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+}
 
 export default cloudinary;
